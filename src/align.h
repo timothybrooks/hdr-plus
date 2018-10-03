9ee6f6d060590bb4481b81771f4308f8e5ffaf42
KillerInk <KillerInk@email.de>
2018-10-03 17:51:40 +0200

    remove factor and lower min/max-dist.
    increase MIN/MAX_OFFSET clamp

--- src/align.h
+++ src/align.h
@@ -4,8 +4,8 @@
 #define T_SIZE 32           // Size of a tile in the bayer mosaiced image
 #define T_SIZE_2 16         // Half of T_SIZE and the size of a tile throughout the alignment pyramid
 
-#define MIN_OFFSET -168     // Min total alignment (based on three levels and downsampling by 4)
-#define MAX_OFFSET 126      // Max total alignment. Differs from MIN_OFFSET because total search range is 8 for better vectorization
+#define MIN_OFFSET -256     // Min total alignment (based on three levels and downsampling by 4)
+#define MAX_OFFSET 256      // Max total alignment. Differs from MIN_OFFSET because total search range is 8 for better vectorization
 
 #define DOWNSAMPLE_RATE 4   // Rate at which layers of the alignment pyramid are downsampled relative to each other
 
