fd9dd0c6844bd7b0a030e690aad1663657b91f34
Titaniumtown <11786225+Titaniumtown@users.noreply.github.com>
2019-03-30 19:34:49 -0400

    Update HDRPlus.cpp

--- src/HDRPlus.cpp
+++ src/HDRPlus.cpp
@@ -1,125 +1,131 @@
 #include "Halide.h"
 #include "halide_load_raw.h"
-
+#include "libraw/libraw.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../include/stb_image_write.h"
-
+#include <stdio.h>
 #include "align.h"
 #include "merge.h"
 #include "finish.h"
+#include <iostream>
+#include <fstream>
 
 using namespace Halide;
-
+using namespace std;
 /*
  * HDRPlus Class -- Houses file I/O, defines pipeline attributes and calls
  * processes main stages of the pipeline.
  */
 class HDRPlus {
+    
+private:
+    
+    const Buffer<uint16_t> imgs;
+    
+public:
 
-    private:
-
-        const Buffer<uint16_t> imgs;
-
-    public:
-
-        // dimensions of pixel phone output images are 3036 x 4048
-
-        static const int width = 5796;
-        static const int height = 3870;
-
-        const BlackPoint bp;
-        const WhitePoint wp;
-        const WhiteBalance wb;
-        const Compression c;
-        const Gain g;
-
-        HDRPlus(Buffer<uint16_t> imgs, BlackPoint bp, WhitePoint wp, WhiteBalance wb, Compression c, Gain g) : imgs(imgs), bp(bp), wp(wp), wb(wb), c(c), g(g) {
-
-            assert(imgs.dimensions() == 3);         // width * height * img_idx
-            assert(imgs.width() == width);
-            assert(imgs.height() == height);
-            assert(imgs.extent(2) >= 2);            // must have at least one alternate image
-        }
-
-        /*
-         * process -- Calls all of the main stages (align, merge, finish) of the pipeline.
-         */
-        Buffer<uint8_t> process() {
-
-            Func alignment = align(imgs);
-            Func merged = merge(imgs, alignment);
-            Func finished = finish(merged, width, height, bp, wp, wb, c, g);
-
-            ///////////////////////////////////////////////////////////////////////////
-            // realize image
-            ///////////////////////////////////////////////////////////////////////////
-
-            Buffer<uint8_t> output_img(3, width, height);
-
-            finished.realize(output_img);
-
-            // transpose to account for interleaved layout
-
-            output_img.transpose(0, 1);
-            output_img.transpose(1, 2);
-
-            return output_img;
-        }
-
-        /*
-         * load_raws -- Loads CR2 (Canon Raw) files into a Halide Image.
-         */
-        static bool load_raws(std::string dir_path, std::vector<std::string> &img_names, Buffer<uint16_t> &imgs) {
-
-            int num_imgs = img_names.size();
-
-            imgs = Buffer<uint16_t>(width, height, num_imgs);
-
-            uint16_t *data = imgs.data();
-
-            for (int n = 0; n < num_imgs; n++) {
-
-                std::string img_name = img_names[n];
-                std::string img_path = dir_path + "/" + img_name;
-
-                if(!Tools::load_raw(img_path, data, width, height)) {
-
-                    std::cerr << "Input image failed to load" << std::endl;
-                    return false;
-                }
-
-                data += width * height;
-            }
-            return true;
-        }
-
-        /*
-         * save_png -- Writes an interleaved Halide image to an output file.
-         */
-        static bool save_png(std::string dir_path, std::string img_name, Buffer<uint8_t> &img) {
-
+    
+    static const int width;
+    static const int height;
+    
+    const BlackPoint bp;
+    const WhitePoint wp;
+    const WhiteBalance wb;
+    const Compression c;
+    const Gain g;
+    char *readfile;
+    std::string readfilestring;
+    int imagesize();
+    
+    HDRPlus(Buffer<uint16_t> imgs, BlackPoint bp, WhitePoint wp, WhiteBalance wb, Compression c, Gain g) : imgs(imgs), bp(bp), wp(wp), wb(wb), c(c), g(g) {
+        
+        assert(imgs.dimensions() == 3);         // width * height * img_idx
+        assert(imgs.width() == width);
+        assert(imgs.height() == height);
+        assert(imgs.extent(2) >= 2);            // must have at least one alternate image
+    }
+    
+    /*
+     * process -- Calls all of the main stages (align, merge, finish) of the pipeline.
+     */
+    Buffer<uint8_t> process() {
+        
+        Func alignment = align(imgs);
+        Func merged = merge(imgs, alignment);
+        Func finished = finish(merged, width, height, bp, wp, wb, c, g);
+        
+        ///////////////////////////////////////////////////////////////////////////
+        // realize image
+        ///////////////////////////////////////////////////////////////////////////
+        
+        Buffer<uint8_t> output_img(3, width, height);
+        
+        finished.realize(output_img);
+        
+        // transpose to account for interleaved layout
+        
+        output_img.transpose(0, 1);
+        output_img.transpose(1, 2);
+        
+        return output_img;
+    }
+    
+    /*
+     * load_raws -- Loads CR2 (Canon Raw) files into a Halide Image.
+     */
+    static bool load_raws(std::string dir_path, std::vector<std::string> &img_names, Buffer<uint16_t> &imgs) {
+        
+        int num_imgs = img_names.size();
+        
+        imgs = Buffer<uint16_t>(width, height, num_imgs);
+        
+        uint16_t *data = imgs.data();
+        
+        for (int n = 0; n < num_imgs; n++) {
+            
+            std::string img_name = img_names[n];
             std::string img_path = dir_path + "/" + img_name;
-
-            std::remove(img_path.c_str());
-
-            int stride_in_bytes = img.width() * img.channels();
-
-            if(!stbi_write_png(img_path.c_str(), img.width(), img.height(), img.channels(), img.data(), stride_in_bytes)) {
-
-                std::cerr << "Unable to write output image '" << img_name << "'" << std::endl;
+            
+            if(!Tools::load_raw(img_path, data, width, height)) {
+                
+                std::cerr << "Input image failed to load" << std::endl;
                 return false;
             }
-
-            return true;
+            
+            data += width * height;
         }
+        return true;
+    }
+    
+    /*
+     * save_png -- Writes an interleaved Halide image to an output file.
+     */
+    static bool save_png(std::string dir_path, std::string img_name, Buffer<uint8_t> &img) {
+        
+        std::string img_path = dir_path + "/" + img_name;
+        
+        std::remove(img_path.c_str());
+        
+        int stride_in_bytes = img.width() * img.channels();
+        
+        if(!stbi_write_png(img_path.c_str(), img.width(), img.height(), img.channels(), img.data(), stride_in_bytes)) {
+            
+            std::cerr << "Unable to write output image '" << img_name << "'" << std::endl;
+            return false;
+        }
+        
+        return true;
+    }
 };
+
+
 
 /*
  * read_white_balance -- Reads white balance multipliers from file and returns WhiteBalance.
  */
 const WhiteBalance read_white_balance(std::string file_path) {
 
-    Tools::Internal::PipeOpener f(("../tools/dcraw -v -i " + file_path).c_str(), "r");
+    Tools::Internal::PipeOpener f(("../tools/dcraw_macos -v -i " + file_path).c_str(), "r");
     
     char buf[1024];
 
@@ -139,6 +145,18 @@ const WhiteBalance read_white_balance(std::string file_path) {
 
     return {1, 1, 1, 1};
 }
+
+
+
+int imagesize(char *FILE) {
+    LibRaw iProcessor;
+
+    iProcessor.open_file(read_file);
+    static const int width = iProcessor.imgdata.sizes.width;
+    static const int height = iProcessor.imgdata.sizes.height;
+    iProcessor.recycle();
+}
+
 
 int main(int argc, char* argv[]) {
     
@@ -182,15 +200,21 @@ int main(int argc, char* argv[]) {
 
     std::vector<std::string> in_names;
 
+    std::string readfilestring = dir_path + "/" + in_names[0];
+    char *readfile = read_file_string.c_str();    
+    int imagesize(readfile);
+    
+    
+    const WhiteBalance wb = read_white_balance(dir_path + "/" + in_names[0]);
+    
     while (i < argc) in_names.push_back(argv[i++]);
 
     Buffer<uint16_t> imgs;
 
     if(!HDRPlus::load_raws(dir_path, in_names, imgs)) return -1;
 
-    const WhiteBalance wb = read_white_balance(dir_path + "/" + in_names[0]);
-    const BlackPoint bp = 2050;
-    const WhitePoint wp = 15464;
+    const BlackPoint bp = 4095;
+    const WhitePoint wp = 255;
 
     HDRPlus hdr_plus = HDRPlus(imgs, bp, wp, wb, c, g);
 
