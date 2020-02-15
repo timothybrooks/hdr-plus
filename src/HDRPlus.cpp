17bd16de2ddf32ea53acf1f74297fa7bce5ddd91
brotherofken <brotherofken@gmail.com>
2020-02-15 12:25:38 +0300

    Revert whitespaces

--- src/HDRPlus.cpp
+++ src/HDRPlus.cpp
@@ -19,17 +19,17 @@ using namespace std;
  * processes main stages of the pipeline.
  */
 class HDRPlus {
-
+    
 private:
-
+    
     const Halide::Runtime::Buffer<uint16_t> imgs;
-
+    
 public:
 
-
+    
     const int width;
     const int height;
-
+    
     const BlackPoint bp;
     const WhitePoint wp;
     const WhiteBalance wb;
@@ -38,7 +38,7 @@ public:
     char *readfile;
     std::string readfilestring;
     void imagesize();
-
+    
     HDRPlus(Halide::Runtime::Buffer<uint16_t> imgs, BlackPoint bp, WhitePoint wp, WhiteBalance wb, Compression c, Gain g)
         : imgs(imgs)
         , width(imgs.width())
@@ -52,7 +52,7 @@ public:
         assert(imgs.dimensions() == 3);         // width * height * img_idx
         assert(imgs.extent(2) >= 2);            // must have at least one alternate image
     }
-
+    
     /*
      * process -- Calls all of the main stages (align, merge, finish) of the pipeline.
      */
@@ -62,47 +62,47 @@ public:
         Func alignment = align(imgsBuffer);
         Func merged = merge(imgsBuffer, alignment);
         Func finished = finish(merged, width, height, bp, wp, wb, c, g);
-
+        
         ///////////////////////////////////////////////////////////////////////////
         // realize image
         ///////////////////////////////////////////////////////////////////////////
-
+        
         Buffer<uint8_t> output_img(3, width, height);
-        finished.compile_jit();
+        
         finished.realize(output_img);
-
+        
         // transpose to account for interleaved layout
-
+        
         output_img.transpose(0, 1);
         output_img.transpose(1, 2);
-
+        
         return output_img;
     }
-
+    
     /*
      * save_png -- Writes an interleaved Halide image to an output file.
      */
     static bool save_png(std::string dir_path, std::string img_name, Buffer<uint8_t> &img) {
-
+        
         std::string img_path = dir_path + "/" + img_name;
-
+        
         std::remove(img_path.c_str());
-
+        
         int stride_in_bytes = img.width() * img.channels();
-
+        
         if(!stbi_write_png(img_path.c_str(), img.width(), img.height(), img.channels(), img.data(), stride_in_bytes)) {
-
+            
             std::cerr << "Unable to write output image '" << img_name << "'" << std::endl;
             return false;
         }
-
+        
         return true;
     }
 };
 
 
 int main(int argc, char* argv[]) {
-
+    
     if (argc < 5) {
         std::cerr << "Usage: " << argv[0] << " [-c comp -g gain (optional)] dir_path out_img raw_img1 raw_img2 [...]" << std::endl;
         return 1;
